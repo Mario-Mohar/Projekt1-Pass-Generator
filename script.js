118b6ac66672a51379d60125c724d87a844dcac9
Mario-Mohar <iam@themo.at>
2023-06-26 10:06:49 +0200

    Finish the Project

--- script.js
+++ script.js
@@ -53,3 +53,17 @@ const updateSlider = () => {
 }
 updateSlider();
 
+const copyPassword = () => {
+    navigator.clipboard.writeText(passwordInput.value);
+    copyIcon.innerText = "check";
+    copyIcon.style.color = "#4285f4";
+    setTimeout(() => {
+        copyIcon.innerText = "copy_all";
+        copyIcon.style.color = "#707070";
+    }, 1500);
+    }
+
+    copyIcon.addEventListener("click", copyPassword);
+    lengthSlider.addEventListener("input", updateSlider);
+    generateBtn.addEventListener("click", generatePassword);
+    
